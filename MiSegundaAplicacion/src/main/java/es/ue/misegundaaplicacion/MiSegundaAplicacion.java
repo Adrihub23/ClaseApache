60b943b0e710918e1e3ba299664c0c9d4db5359f
Adrihub23 <adrian.ocanadefrutos@gmail.com>
2024-09-19 09:59:30 +0200

    Primer commit de mi SegundaAplicacion

---
+++ MiSegundaAplicacion/src/main/java/es/ue/misegundaaplicacion/MiSegundaAplicacion.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package es.ue.misegundaaplicacion;
+
+/**
+ *
+ * @author Adrian
+ */
+public class MiSegundaAplicacion {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
